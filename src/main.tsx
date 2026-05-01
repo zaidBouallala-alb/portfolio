b57bfd9392727d58f288dc69dedaee20364e2b4a
zaidBouallala <zaidbouallala7@gmail.com>
2026-05-01 08:14:00 +0100

    feat: scaffold portfolio application with core components, theme toggle, and i18n support

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import './i18n/i18n'
 import './index.css'
 import App from './App.tsx'
 import { ThemeProvider } from './context/ThemeContext.tsx'
