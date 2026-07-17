0a78aa59ecf68c613afc96387e7d5096ad9d94ea
zaidBouallala <zaidbouallala7@gmail.com>
2026-07-17 20:22:27 +0100

    feat: implement internationalization support with English, French, and Spanish localization files

--- src/i18n/i18n.ts
+++ src/i18n/i18n.ts
@@ -4,6 +4,7 @@ import LanguageDetector from 'i18next-browser-languagedetector';
 
 import en from './locales/en.json';
 import fr from './locales/fr.json';
+import es from './locales/es.json';
 
 i18n
   .use(LanguageDetector)
@@ -12,6 +13,7 @@ i18n
     resources: {
       en: { translation: en },
       fr: { translation: fr },
+      es: { translation: es },
     },
     fallbackLng: 'en',
     interpolation: {
